51e7f186c9c0bcc92b2cbd14483a8772dfc46d2c
angelus008 <bury.andrzej@poczta.fm>
2017-06-19 15:19:06 +0200

    test jednostkowy

--- kolos/test punktu.cs
+++ kolos/test punktu.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace kolos
 {
-    class test_punktu
-    {
+ 
+        [TestClass]
+        public class PointTests
+        {
+            [TestMethod]
+            public void PointCreateTest()
+            {
+                try
+                {
+                    Point point = new Point(2, 5);
+                }
+                catch (Exception)
+                {
+                    Assert.Fail();
+                }
+            }
+
+            [TestMethod]
+            public void GetPositionTest()
+            {
+                Point point = new Point(2, 5);
+
+                Assert.AreEqual(point.X, 2);
+                Assert.AreEqual(point.Y, 5);
+            }
+        }
     }
 }
+
