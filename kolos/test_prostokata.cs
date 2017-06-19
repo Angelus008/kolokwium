81975a235d14a272f5bfdd0f888b323944598db7
angelus008 <bury.andrzej@poczta.fm>
2017-06-19 15:42:48 +0200

    test jednostkowy prostokąta

--- kolos/test_prostokata.cs
+++ kolos/test_prostokata.cs
@@ -8,7 +8,33 @@ using kolos.Model;
 
 namespace kolos
 {
+    [TestClass]
     class test_prostokata
     {
+       
+        
+        {
+            [TestMethod]
+            public void PointCreateTest()
+            {
+                try
+                {
+                    Point point = new Point(2, 5);
+                }
+                catch (Exception)
+                {
+                    Assert.Fail();
+                }
+            }
+
+            [TestMethod]
+            public void GetPositionTest()
+            {
+                Point point = new Point(2, 5);
+
+                Assert.AreEqual(point.X, 2);
+                Assert.AreEqual(point.Y, 5);
+            }
+
+        }
     }
-}
